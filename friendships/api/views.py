bababf9886d8cab69a4a55035f4e3b2a5a0f41dd
Bluelotus828 <yuguanlin828@gmail.com>
2023-12-24 21:28:32 -0800

    tests and database migtations

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -36,10 +36,6 @@ class FriendshipViewSet(viewsets.GenericViewSet):
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
-        self.get_object()
-        # api/friendships/pk(to_user_id)/follow
-        # 特殊判断重复 follow 的情况（比如前端猛点好多少次 follow)
-        # 静默处理，不报错，因为这类重复操作因为网络延迟的原因会比较多，没必要当做错误处理
         if Friendship.objects.filter(from_user=request.user, to_user=pk).exists():
             return Response({
                 'success': True,
@@ -59,7 +55,6 @@ class FriendshipViewSet(viewsets.GenericViewSet):
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
-        self.get_object()
         # 注意 pk 的类型是 str，所以要做类型转换
         if request.user.id == int(pk):
             return Response({
