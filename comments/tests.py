25c701e998c20a57ec8919ed3d6f3a72ba1b928b
Bluelotus828 <yuguanlin828@gmail.com>
2023-12-31 00:56:45 -0800

    likes models and admin

--- comments/tests.py
+++ comments/tests.py
@@ -1,10 +1,25 @@
 from testing.testcases import TestCase
+from datetime import timedelta
+from utils.time_helpers import utc_now
 
 
-class CommentModelTests(TestCase):
+class TweetTests(TestCase):
+    def setUp(self):
+        self.linghu = self.create_user('linghu')
+        self.tweet = self.create_tweet(self.linghu, content='Jiuzhang Dafa Hao')
 
-    def test_comment(self):
-        user = self.create_user('JohnDoe')
-        tweet = self.create_tweet(user)
-        comment = self.create_comment(user, tweet)
-        self.assertNotEqual(comment.__str__(), None)
+    def test_hours_to_now(self):
+        self.tweet.created_at = utc_now() - timedelta(hours=10)
+        self.tweet.save()
+        self.assertEqual(self.tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        self.create_like(self.linghu, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        dongxie = self.create_user('dongxie')
+        self.create_like(dongxie, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
