0465bd222704ce8a913b9cb721d303ccd86a0391
Bluelotus828 <yuguanlin828@gmail.com>
2024-01-11 00:07:00 -0800

    tweets pagination, endless pagination

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -9,7 +9,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from utils.paginations import FriendshipPagination
+from friendships.api.paginations import FriendshipPagination
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
