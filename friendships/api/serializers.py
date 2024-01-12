0f6df092ba1df57b2259e401a3870107990a6615
Bluelotus828 <yuguanlin828@gmail.com>
2024-01-12 00:18:53 -0800

    include memcached to project

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -5,6 +5,21 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 
+class FollowingUserIdSetMixin:
+
+    @property
+    def following_user_id_set(self: serializers.ModelSerializer):
+        if self.context['request'].user.is_anonymous:
+            return {}
+        if hasattr(self, '_cached_following_user_id_set'):
+            return self._cached_following_user_id_set
+        user_id_set = FriendshipService.get_following_user_id_set(
+            self.context['request'].user.id,
+        )
+        setattr(self, '_cached_following_user_id_set', user_id_set)
+        return user_id_set
+
+
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
     from_user_id = serializers.IntegerField()
     to_user_id = serializers.IntegerField()
@@ -16,14 +31,7 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs['from_user_id'] == attrs['to_user_id']:
             raise ValidationError({
-                'message': 'You can not follow yourself'
-            })
-        if Friendship.objects.filter(
-                from_user_id=attrs['from_user_id'],
-                to_user_id=attrs['to_user_id'],
-        ).exists():
-            raise ValidationError({
-                'message': 'You already followed this friend'
+                'message': 'from_user_id and to_user_id should be different'
             })
         return attrs
 
@@ -39,39 +47,27 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
 # 可以通过 source=xxx 指定去访问每个 model instance 的 xxx 方法
 # 即 model_instance.xxx 来获得数据
 # https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
-class FollowerSerializer(serializers.ModelSerializer):
+class FollowerSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
     user = UserSerializerForFriendship(source='from_user')
-    # this source can also be a function, not necessarily a field
     created_at = serializers.DateTimeField()
     has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at', 'has_followed',)
+        fields = ('user', 'created_at', 'has_followed')
 
     def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
-        # 我们将在后序的课程中解决这个问题
-        return FriendshipService.has_followed(self.context['request'].user,
-                                              obj.from_user)
+        return obj.from_user_id in self.following_user_id_set
 
 
-class FollowingSerializer(serializers.ModelSerializer):
+class FollowingSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
     user = UserSerializerForFriendship(source='to_user')
     created_at = serializers.DateTimeField()
     has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at', 'has_followed',)
+        fields = ('user', 'created_at', 'has_followed')
 
     def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-
-        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
-        # 我们将在后序的课程中解决这个问题
-        return FriendshipService.has_followed(self.context['request'].user,
-                                              obj.to_user)
+       return obj.to_user_id in self.following_user_id_set
