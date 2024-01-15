300ebd264c8e40cdb1a3d8c7bd282e50d824b6b4
Bluelotus828 <yuguanlin828@gmail.com>
2024-01-14 19:45:55 -0800

    tweet cache

--- likes/models.py
+++ likes/models.py
@@ -1,9 +1,10 @@
+from accounts.services import UserService
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from utils.memcached_helper import MemcachedHelper
 
-from accounts.services import UserService
 
 
 class Like(models.Model):
@@ -43,4 +44,4 @@ class Like(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
